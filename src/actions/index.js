ed2dd26df45a0d4a46ffa92f8147e1dc4148f122
henry <henryyogman@gmail.com>
2016-07-27 21:09:39 +0000

    Sending blog features to repo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST'; 
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=fkjldsajiejiofjoiewjviimejwovit942308540395u43v09nf43u95v4w493utvijrlaiv';
@@ -12,6 +15,33 @@ export function fetchPosts() {
 		payload: request 
 
 	};
+}
 
+export function createPost(props) {
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props); 
+	
+	return {
+		type:CREATE_POST, 
+		payload:request
+	};
+}
 
+export function fetchPost(id) {
+	const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`); 
+	
+	return {
+		type: FETCH_POST,
+		payload:request
+	};
+	
+}
+
+export function deletePost(id) {
+	const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+	
+	return {
+		type:DELETE_POST,
+		payload:request
+		
+	};
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Component} from 'react';
+import { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+      
       {this.props.children}
-      <h1>Working</h1>
    
       </div>
     );
